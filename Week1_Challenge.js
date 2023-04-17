031244c33dcc9121a9afef611ba83902c6737d59
maguy bikene <bikene.maguy@yahoo.com>
2023-04-17 02:53:24 -0500

    week1 challenge cmplt

--- Week1_Challenge.js
+++ Week1_Challenge.js
@@ -1,1 +1,10 @@
+//Write a function that takes the base and height of a triangle and return its area.
+ 
 
+let triBase=5;
+let triHeight=100;
+
+function triArea(triBase, triHeight) {
+ return triBase * triHeight/2};
+ 
+console.log(triArea(triBase, triHeight));
