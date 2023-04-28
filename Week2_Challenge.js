d80efb26dde17c5afc03bdcf19836d8a8cf5113c
maguy bikene <bikene.maguy@yahoo.com>
2023-04-28 00:32:31 -0500

    week3 done

--- Week2_Challenge.js
+++ Week2_Challenge.js
@@ -1,1 +1,18 @@
+//Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+numsArray = [ 1, 2, 3, 4, 5, 6,];
+let min = numsArray[5];
+let max = numsArray[1];
 
+function findMinMax(numsArray) {
+    for (let i = 0; i < numsArray.length; i++) {
+        if (numsArray[i] < min) {
+            min = numsArray[i];
+        }
+        
+        if (numsArray[i] > max) {
+            max = numsArray[i];
+        }
+    }
+    return [min, max];
+}
+    console.log (findMinMax(numsArray));
