7aba49506776fb55caeb21ef291aef2b9cb0377e
maguy bikene <bikene.maguy@yahoo.com>
2023-04-28 00:32:54 -0500

    week 3 done!

--- Week3_Challenge.js
+++ Week3_Challenge.js
@@ -1,1 +1,11 @@
+//Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
 
+function FirstReverse(str) {
+    return str.split("").reverse().join("");
+    }
+    
+    console.log(FirstReverse("Hello World and Coders"));
+
+    // split function splits a string into an array of substrings.
+    //reverse function reverses the order of the substrings in the array.
+    // join function joins the array of substrings back into a string.
